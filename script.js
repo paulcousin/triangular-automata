c714fadc9f07cb1214fd5b2db603deb8585869ea
Paul Cousin <cousin.paul@gmail.com>
2024-05-21 14:26:49 +0200

    Improve home page

--- script.js
+++ script.js
@@ -56,3 +56,16 @@ function loadLargeImage(imageId, loadingImagePath, largeImagePath) {
       
       lastScrollTop = st;
   }
+
+//// Formats a string to look like a title
+  function titleCase(str) {
+    var splitStr = str.toLowerCase().replaceAll('-', ' ').split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        // You do not need to check if i is larger than splitStr length, as your for does that for you
+        // Assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    // Directly return the joined string
+    return splitStr.join(' '); 
+ }
+ 
